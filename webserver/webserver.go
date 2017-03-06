f5201877816cf50738d816784f67595a8ff7552e
DPini <alumne@a5s112pc03.fib.upc.es>
2017-03-06 12:43:40 +0100

    Added routes to our webserver

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -2,16 +2,26 @@ package main
 
 import (
     "fmt"
-    "html"
     "log"
     "net/http"
+    "github.com/gorilla/mux"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
 
-    log.Fatal(http.ListenAndServe(":8080", nil))
+router := mux.NewRouter().StrictSlash(true)
+router.HandleFunc("/", Index)
+router.HandleFunc("/endpoint/{param}", endpointFunc)
 
-} 
+log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Service OK")
+}
+
+func endpointFunc(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    param := vars["param"]
+    fmt.Fprintln(w, "You are calling with param:", param)
+}
